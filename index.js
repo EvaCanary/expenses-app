c2573be1ba42d456eac0a04b72e24dd60a647145
Eva <145874312+EvaCanary@users.noreply.github.com>
2024-02-19 15:48:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+const expenses = [];
+const inputNode = document.querySelector('.js-input');
+const buttonNode = document.querySelector('.js-button');
+const historyNode = document.querySelector('.js-history');
+buttonNode.addEventListener('click', function() {
+//если ничего нет в поле ввода - вернуть
+// 1. получаем значение из поля воода
+if (!inputNode.value) {
+    return;
+}
+console.log(inputNode.value);
+// ввод пользователя передается в функцию
+ const expense = parseInt(inputNode.value);
+ // сброс поля ввода после внесения
+ inputNode.value = '';
+// внесение данных по кнопке
+// 2. сохраняем трату в список
+ expenses.push(expense);
+// 3. выводим новый список трат
+const html = '<ol><li>expense</li></ol>';
+historyNode.innerHTML = html;
+});
+
+

063d377e80a60530aa0bd18e5e5760cfcaeb47e8
Eva <145874312+EvaCanary@users.noreply.github.com>
2024-02-28 11:07:43 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+const LIMIT = 20000;
+
 const expenses = [];
+
 const inputNode = document.querySelector('.js-input');
 const buttonNode = document.querySelector('.js-button');
 const historyNode = document.querySelector('.js-history');
+const sumNode = document.querySelector('.js-sum');
+const limitNode = document.querySelector('.js-limit');
+const statusNode = document.querySelector('.js-status')
+
+limitNode.innerText = LIMIT;
 buttonNode.addEventListener('click', function() {
 //если ничего нет в поле ввода - вернуть
 // 1. получаем значение из поля воода
@@ -16,9 +24,31 @@ console.log(inputNode.value);
 // внесение данных по кнопке
 // 2. сохраняем трату в список
  expenses.push(expense);
+
 // 3. выводим новый список трат
-const html = `<ol><li>${expense}</li></ol>`;
-historyNode.innerHTML = html;
+let expensesListHTML = '';
+
+expenses.forEach(element => {
+    expensesListHTML += `<li>${element} руб.</li>`;
+});
+
+historyNode.innerHTML = `<ol>${expensesListHTML}</ol>`;
+//4. посчитать сумму и вывести ее
+let sum = 0;
+
+expenses.forEach(element => {
+    sum += element;
+});
+
+sumNode.innerText = sum;
+//5. Сравнение с лимитом и вывод статуса
+if (sum <= LIMIT) {
+    statusNode.innerText = 'все хорошо';
+    statusNode.classList.add('status_blue');
+} else {
+    statusNode.innerText = 'все плохо';
+    statusNode.classList.add('status_red');
+}
 });
 
 
